315d9aa7d5e4289f09fd955e868b7cb1761bf981
Lucy Stevens <lcm.stevens96@gmail.com>
2022-12-10 11:18:27 +0000

    Day 10 solution

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -1,33 +1,74 @@
 package day10
 
-import InputType
-import ReturnType
 import common.Challenge
 import common.Part
 import common.tests
+import groupByLines
+import print
+import kotlin.math.abs
 
-class Day10 : Challenge<InputType, ReturnType>() {
+class Day10 : Challenge<List<String>, Int>() {
     override val day: String = "day10"
 
-    override fun part1(input: InputType): ReturnType {
+    fun List<String>.parseCommands(): List<Int> =
+        listOf(0) + flatMap {
+            if(it.startsWith("addx"))
+                listOf(0, it.substringAfter(" ").toInt())
+            else listOf(0)
+        }
+
+    fun <T> List<T>.mapwithLast(defaultVal: T, mapper: (T, T) -> T): List<T> {
+        var last = defaultVal
+        return map { item ->
+            mapper(last, item).also { last = it }
+        }
+    }
+
+    override fun part1(input: List<String>): Int {
+        return input.parseCommands()
+            .mapwithLast(1){ last, item ->
+                last + item
+            }
+            .let {
+                it.strength(20) +
+                        it.strength(60) +
+                        it.strength(100) +
+                        it.strength(140) +
+                        it.strength(180) +
+                        it.strength(220)
+            }
+    }
+
+    fun List<Int>.strength(cycle: Int) =
+        get(cycle-1)*cycle
+
+    override fun part2(input: List<String>): Int {
+        input.parseCommands()
+            .mapwithLast(1){ last, item ->
+                last + item
+            }.mapIndexed { idx, x ->
+                val cycle = idx-2
+                val y = (idx) % 40
+                if(abs(x-y)<=1) "#" else "."
+            }.groupByLines(40)
+            .map { it.joinToString("") }
+            .print()
+
         return 0
     }
 
-    override fun part2(input: InputType): ReturnType {
-        return 0
-    }
-
-    override fun parseInput(lines: List<String>): InputType {
+    override fun parseInput(lines: List<String>): List<String> {
         return lines
     }
 
-    override val tests = tests<ReturnType> {
-        file("test", part1 = 0, part2 = 0)
-        file("input")
+    // part2 = "EGLHBLFJ"
+    override val tests = tests<Int> {
+        file("test", part1 = 13140)
+        file("input", part1 = 13920)
     }
 
 }
 
 fun main() {
-    Day10().test(Part.ONE)
+    Day10().test(Part.BOTH)
 }

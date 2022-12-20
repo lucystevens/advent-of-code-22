4f45cfee98399a4952a068c3df60b54a3eb34eaa
lucystevens <lucystevens@users.noreply.github.com>
2022-12-20 05:21:21 +0000

    Generated files for day 20

---
+++ src/day20/Day20.kt
@@ -0,0 +1,33 @@
+package day20
+
+import InputType
+import ReturnType
+import common.Challenge
+import common.Part
+import common.tests
+
+class Day20 : Challenge<InputType, ReturnType>() {
+    override val day: String = "day20"
+
+    override fun part1(input: InputType): ReturnType {
+        return 0
+    }
+
+    override fun part2(input: InputType): ReturnType {
+        return 0
+    }
+
+    override fun parseInput(lines: List<String>): InputType {
+        return lines
+    }
+
+    override val tests = tests<ReturnType> {
+        file("test", part1 = 0, part2 = 0)
+        file("input")
+    }
+
+}
+
+fun main() {
+    Day20().test(Part.ONE)
+}
